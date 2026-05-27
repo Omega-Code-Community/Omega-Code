abe20813fe588b52c3ac1c5ebf94eb28499f3337
pengfeiqq12 <2756757632@qq.com>
2026-05-27 21:46:56 +0800

    chore: 添加启动类注释

--- console/console-panel/src/main/java/code/omega/OmegaConsolePanelApplication.java
+++ console/console-panel/src/main/java/code/omega/OmegaConsolePanelApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OmegaConsolePanelApplication {
 
+	// 启动类
 	public static void main(String[] args) {
 		SpringApplication.run(OmegaConsolePanelApplication.class, args);
 	}
